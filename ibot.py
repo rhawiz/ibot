2af615ec2b8d837f0ffd066a70886a3360ba6795
Rawand Hawiz <r.hawiz@geophy.com>
2018-11-07 14:29:47 +0100

    additional args

--- ibot.py
+++ ibot.py
@@ -1,40 +1,10 @@
-import json
 import random
 import sys
 from time import sleep
 
-from instagram_private_api import Client, ClientCompatPatch
+from instagram_private_api import Client
 
 from ids import USER_IDS
-
-
-def follow(api, max=2000):
-    c = 0
-    while True:
-        rank_token = api.generate_uuid(False)
-        user_id = random.choice(USER_IDS)
-        user_followers = api.user_followers(user_id=user_id, rank_token=rank_token)
-        for user in user_followers["users"]:
-            c += 1
-            _id = user["pk"]
-            wait = 1
-            if not c % 100:
-                print("100 follow requests sent, sleeping for 2.5 mins")
-                sleep(180)
-            while True:
-                try:
-                    resp = api.friendships_create(_id)
-                    wait = 0
-                    break
-
-                except Exception as e:
-                    print("Going to fast...sleeping {}".format(wait * 10))
-                    sleep(wait * 10)
-                    wait += 1
-            print(c, "Following", resp)
-            if c >= max:
-                return
-            sleep(random.randint(1, 4))
 
 
 def get_all_followers(api):
@@ -53,17 +23,63 @@ def get_all_followers(api):
     return followers
 
 
-def unfollow(api, max=None):
+def follow(api, interval=1800, action_interval=4, rate=40, limit=2000):
     c = 0
+    if not interval:
+        interval = 1800
+    if not action_interval:
+        action_interval = 4
+    if not rate:
+        rate = 40
+    if not limit:
+        limit = 2000
+    while True:
+        rank_token = api.generate_uuid(False)
+        user_id = random.choice(USER_IDS)
+        user_followers = api.user_followers(user_id=user_id, rank_token=rank_token)
+        for user in user_followers["users"]:
+            c += 1
+            _id = user["pk"]
+            wait = 10
+            if not c % rate:
+                print("{} follow requests sent, sleeping for {} mins".format(rate, interval / 60))
+                sleep(interval)
+            while True:
+                try:
+                    resp = api.friendships_create(_id)
+                    wait = 0
+                    break
+
+                except Exception as e:
+                    print(e)
+                    print("Going to fast...sleeping {} seconds".format(wait))
+                    sleep(wait)
+                    wait = wait * 2
+            print(c, "Following", resp)
+            if c >= limit:
+                return
+
+            sleep(action_interval + random.uniform(action_interval * -0.5, action_interval * 0.5))
+
+
+def unfollow(api, interval=1800, action_interval=4, rate=40, limit=None):
+    c = 0
+
+    if not interval:
+        interval = 1800
+    if not action_interval:
+        action_interval = 4
+    if not rate:
+        rate = 40
 
     followers = get_all_followers(api)
 
     for id in followers:
         c += 1
-        wait = 1
-        if not c % 100:
-            print("100 unfollow requests sent, sleeping for 2.5 mins")
-            sleep(180)
+        wait = 10
+        if not c % rate:
+            print("{} unfollow requests sent, sleeping for {} mins".format(rate, interval / 60))
+            sleep(interval)
         while True:
             try:
                 resp = api.friendships_destroy(id)
@@ -71,29 +87,42 @@ def unfollow(api, max=None):
                 break
 
             except Exception as e:
-                print("Going to fast...sleeping {}".format(wait * 10))
-                sleep(wait * 10)
-                wait += 1
+                print(e)
+                print("Going to fast...sleeping {} seconds".format(wait))
+                sleep(wait)
+                wait = wait * 2
 
         print(c, "Unfollow", resp)
-        if max and c >= max:
+        if limit and c >= limit:
             return
-        sleep(random.randint(1, 4))
+        sleep(action_interval + random.uniform(action_interval * -0.5, action_interval * 0.5))
 
 
 def main():
     if len(sys.argv) >= 3:
         user_name = sys.argv[1]
         password = sys.argv[2]
+        interval = None
+        action_interval = None
+        rate = None
+        if len(sys.argv) == 4:
+            interval = int(sys.argv[3])
+        if len(sys.argv) == 5:
+            action_interval = int(sys.argv[4])
+        if len(sys.argv) == 6:
+            rate = int(sys.argv[5])
     else:
         user_name = input("Username:")
         password = input("Password:")
+        interval = int(input("Interval in seconds (wait time between 40 requests):"))
+        action_interval = int(input("Action Interval (wait time between each request):"))
+        rate = int(input("Rate (amount of requests between each interval):"))
 
     api = Client(user_name, password)
-
+    print(">> Logged in with user '{}'".format(user_name))
     while True:
-        follow(api, 2000)
-        unfollow(api)
+        follow(api, interval=interval, action_interval=action_interval, rate=rate, limit=2000)
+        unfollow(api, interval=interval, action_interval=action_interval, rate=rate, limit=None)
 
 
 if __name__ == "__main__":
