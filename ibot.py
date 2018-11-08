d0b281ab71267fbb09d9418894bfaaecf5cb122a
Rawand Hawiz <r.hawiz@geophy.com>
2018-11-08 13:03:16 +0100

    Additional Args

--- ibot.py
+++ ibot.py
@@ -24,6 +24,8 @@ def get_all_followers(api):
 
 
 def follow(api, interval=1800, action_interval=4, rate=40, limit=2000):
+    print(">> Follow started following {} users.".format(limit or "all"))
+
     c = 0
     if not interval:
         interval = 1800
@@ -62,7 +64,8 @@ def follow(api, interval=1800, action_interval=4, rate=40, limit=2000):
             sleep(action_interval + random.uniform(action_interval * -0.5, action_interval * 0.5))
 
 
-def unfollow(api, interval=1800, action_interval=4, rate=40, limit=None):
+def unfollow(api, interval=1800, action_interval=4, rate=80, limit=None):
+    print(">> Unfollow started unfollowing {} followers.".format(limit or "all"))
     c = 0
 
     if not interval:
@@ -70,7 +73,7 @@ def unfollow(api, interval=1800, action_interval=4, rate=40, limit=None):
     if not action_interval:
         action_interval = 4
     if not rate:
-        rate = 40
+        rate = 80
 
     followers = get_all_followers(api)
 
@@ -102,35 +105,46 @@ def main():
     if len(sys.argv) >= 3:
         user_name = sys.argv[1]
         password = sys.argv[2]
-        wait_before_start = None
+        wait_before_start = 0
         interval = None
         action_interval = None
+        unfollow_first = False
         rate = None
         if len(sys.argv) == 4:
             wait_before_start = int(sys.argv[3])
         if len(sys.argv) == 5:
-            interval = int(sys.argv[4])
+            unfollow_first = True if sys.argv[4] else False
         if len(sys.argv) == 6:
-            action_interval = int(sys.argv[5])
+            interval = int(sys.argv[5])
         if len(sys.argv) == 7:
-            rate = int(sys.argv[6])
+            action_interval = int(sys.argv[6])
+        if len(sys.argv) == 8:
+            rate = int(sys.argv[7])
 
     else:
         user_name = input("Username:")
         password = input("Password:")
-        wait_before_start = int(input("Time to wait before starting bot in seconds:"))
+        wait_before_start = int(input("Time to wait before starting bot in seconds:") or 0)
+        unfollow_first = True if input("Unfollow all first:") else False
         interval = int(input("Interval in seconds (wait time between 40 requests):"))
         action_interval = int(input("Action Interval (wait time between each request):"))
         rate = int(input("Rate (amount of requests between each interval):"))
 
     print(">> Bot started. Waiting {} seconds before starting.".format(wait_before_start))
+    print(">> Unfollow First: {}".format(unfollow_first))
+    print(">> Interval: {}".format(interval))
+    print(">> Action Interval: {}".format(action_interval))
+    print(">> Rate: {}".format(rate))
     sleep(wait_before_start)
 
     api = Client(user_name, password)
     print(">> Logged in with user '{}'".format(user_name))
+
+    f1 = unfollow if unfollow_first else follow
+    f2 = follow if unfollow_first else unfollow
     while True:
-        follow(api, interval=interval, action_interval=action_interval, rate=rate, limit=2000)
-        unfollow(api, interval=interval, action_interval=action_interval, rate=rate, limit=None)
+        f1(api, interval=interval, action_interval=action_interval, rate=rate)
+        f2(api, interval=interval, action_interval=action_interval, rate=rate)
 
 
 if __name__ == "__main__":
